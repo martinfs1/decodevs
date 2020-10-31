21b3a386f4c1e0020758234cf365186edd15aa2b
Martin <martinfernando-sandander@hotmail.com>
2020-10-31 20:45:36 -0300

    nada agregado

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -8,14 +8,14 @@ const FirstView = () => {
     <>
       <NavBar />
       <div className="imgBg d-flex container-fluid align-items-center">
-        <div className="row">
-          <div className="col-sm-8">
+        <div className="row mx-0">
+          <div className="col-md-8">
             <img className="pantallas img-fluid" src={Pantallas} alt="Pantallas" />
           </div>
-          <div className="col-sm-4">
-            <h1 className="title display-4"> &#60;Hablamos/&#62; tu mismo idioma</h1>
+          <div className="col-md-4">
+            <h1 className="title"> &#60;Hablamos/&#62; tu mismo idioma</h1>
             <p className="title">Escuchamos tus inconvenientes y desarrollamos una solución adecuada a tu proyecto.</p>
-            <h1 className="title display-4">Creamos tu aplicación</h1>
+            <h1 className="title">Creamos tu aplicación</h1>
             <p className="title">Para todas tus pantallas. El mundo digital de hoy requiere disponibilidad en todos los soportes.</p>
           </div>
         </div>
