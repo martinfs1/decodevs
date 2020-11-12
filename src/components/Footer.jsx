26fd2b42702ee79b36774d6f18409b811360bc00
Martin <martinfernando-sandander@hotmail.com>
2020-11-11 22:12:10 -0300

    creando nuevo repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import flecha from '../img/flecha.png'
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
+import olasFooter from "../img/olasFooter.png"
 
 const Footer = () => {
   return (
-    <div className=" footer row mx-0 justify-content-around">
-      <div className="text-center col-12 title col-md-6 pt-1 pt-md-5 order-md-1 order-2">
-        <p>Copyright 2020 - Decodevs todos los derechos reservados</p>
-      </div>
-      <div className="position-absolute align-self-end">
-        <Link
-          to="top"
-          spy={true}
-          smooth={true}
-          duration={800}
-        > <img className="flecha" src={flecha} alt="flecha" /></Link>
-      </div>
-      <div className="col-12 col-md-4 order-1 order-md-2" id="contacto">
-        <h2 className=" mt-4 title text-center">Nuestras Redes</h2>
-        <div className="text-center text-md-right pr-md-5 my-3 my-md-0 ">
-          <a href="http://bit.ly/decodevs" target="_blank" className="text-dark text-decoration-none">+ 54 9 381 6261384 <i className="fab redesIcon my-2 fa-whatsapp ml-3 wtsap"></i></a>
-          <br />
-          <a href="https://www.facebook.com/dcdsolucionesweb" target="_blank" className="text-dark text-decoration-none">Decodevs <i className="fab redesIcon my-2 fa-facebook ml-3 fb"></i></a>
-          <br />
-          <a href="!#" className="text-dark text-decoration-none">contacto@decodevs.com <i className="far redesIcon my-2 fa-envelope ml-3 gmail"></i></a>
+    <div>
+      <img className="olas" src={olasFooter} alt="olas" />
+      <div className=" footer row mx-0 justify-content-around">
+        <div className="text-center col-12 title col-md-6 pt-1 pt-md-5 order-md-1 order-2">
+          <p>Copyright 2020 - Decodevs todos los derechos reservados</p>
+        </div>
+        <div className="position-absolute align-self-end">
+          <Link
+            to="top"
+            spy={true}
+            smooth={true}
+            duration={800}
+          > <img className="flecha" src={flecha} alt="flecha" /></Link>
+        </div>
+        <div className="col-12 col-md-4 order-1 order-md-2" id="contacto">
+          <h2 className=" mt-4 title text-center">Nuestras Redes</h2>
+          <div className="text-center text-md-right pr-md-5 my-3 my-md-0 ">
+            <a href="http://bit.ly/decodevs" target="_blank" className="text-dark text-decoration-none">+ 54 9 381 6261384 <i className="fab redesIcon my-2 fa-whatsapp ml-3 wtsap"></i></a>
+            <br />
+            <a href="https://www.facebook.com/dcdsolucionesweb" target="_blank" className="text-dark text-decoration-none">Decodevs <i className="fab redesIcon my-2 fa-facebook ml-3 fb"></i></a>
+            <br />
+            <a href="!#" className="text-dark text-decoration-none">contacto@decodevs.com <i className="far redesIcon my-2 fa-envelope ml-3 gmail"></i></a>
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, rootElement);
+} else {
+  ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, rootElement);
+}
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
